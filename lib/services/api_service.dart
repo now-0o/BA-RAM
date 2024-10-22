bb8e9b597353628f5c3ed08509c345d148667fef
now-0o <dlwpdn0704@gmail.com>
2024-10-22 22:54:21 +0900

    feat: puuid 조회 시 입력한 닉네임 적용되도록 수정

--- lib/services/api_service.dart
+++ lib/services/api_service.dart
@@ -4,11 +4,13 @@ import 'package:http/http.dart' as http;
 
 class ApiService {
   static Future<String> getPuuid(riotId) async {
+    String riotName = riotId.slice("#")[0];
+    String tag = riotId.slice("#")[1];
     String puuid = "";
     const baseUri =
-        'https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id/%EB%8F%99%EA%B7%B8%EB%9D%BC%EB%B0%8D/KR1?api_key=';
+        'https://asia.api.riotgames.com/riot/account/v1/accounts/by-riot-id';
 
-    final url = Uri.parse(baseUri);
+    final url = Uri.parse('$baseUri/$riotName/$tag?api_key=');
     final response = await http.get(url);
     if (response.statusCode == 200) {
       final Map<String, dynamic> riotUserInfo = jsonDecode(response.body);
